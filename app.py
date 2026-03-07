6a74ff129967f3fa08219f17bdd0552d51a515cd
project32639 <project32639@gmail.com>
2026-03-08 00:45:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ background:#a8b1e5;
 }
 
 .model-box{
-background:#B3EBF2;
+background:#OF52BA;
 padding:30px;
 border-radius:15px;
 font-size:20px;
@@ -428,6 +428,7 @@ elif page=="👨‍⚕ Doctor Recommendation":
 
 </div>
 """,unsafe_allow_html=True)
+
 
 
 
